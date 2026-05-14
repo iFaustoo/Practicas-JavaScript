6d694aee25b3d7a21d178e177680b6c85fd42835
iFaustoo <faustoordoriguez10@gmail.com>
2026-05-14 18:44:30 -0300

    -

--- ejercicios_etapa1/ejercicio6.js
+++ ejercicios_etapa1/ejercicio6.js
@@ -1,12 +1,21 @@
-const numeros = [10, 20, 30, 40];
+//FUNCION PARA VALIDAR NOMBRE DE USUARIO
 
-function sumarArray(lista) {
-    let total = 0;
-    
-    for (let i = 0; i < lista.length; i++) {
-        total += [i];
+function validateUsername(input) {
+    if (!input || input.trim().lenght === 0) {
+        return "Error. Nombre vacío/En blanco";
     }
-    return total;
+    if (input.length < 3) {
+        return "Error. No puede ingresar un valor con menos de 3 carácteres";
+    }
+    if (!isNaN(input)) {
+        return "Error. No puede ingresar números";
+    }
 }
 
-console.log(sumarArray(numeros));
+console.log(validateUsername(""));
+console.log(validateUsername("Fa"));
+console.log(validateUsername("12"));
+console.log(validateUsername("123"));
+console.log(validateUsername("Fau"))
+
+//Podría hacerlo con un switch pero alta paja
