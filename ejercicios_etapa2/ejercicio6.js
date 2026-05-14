718eb68b27ce1bfabd3e54e634cf9e34700d18ec
iFaustoo <faustoordoriguez10@gmail.com>
2026-05-14 20:16:07 -0300

    -

---
+++ ejercicios_etapa2/ejercicio6.js
@@ -0,0 +1,13 @@
+//VAMOS CON OBJETOS
+//CREACIÓN Y ACCESO A OBJETOS
+//Creá un objeto llamado persona con las propiedades nombre, edad y profesion.
+//Después, imprimí en consola un mensaje que diga: "Hola, soy [nombre], tengo [edad] años y
+//soy [profesion]" accediendo a las propiedades del objeto.
+
+const persona = {
+    nombre: "Fausto",
+    edad: 18,
+    profesion: "Programador",
+}
+
+console.log(`Hola, me llamo ${persona.nombre}, tengo ${persona.edad} años y estudio para ejercer como ${persona.profesion}`);
