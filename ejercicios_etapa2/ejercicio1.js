403cef59de9fdaccf9aa1f8caed75b4043296504
iFaustoo <faustoordoriguez10@gmail.com>
2026-05-14 23:35:21 -0300

    -

--- ejercicios_etapa2/ejercicio1.js
+++
@@ -1,7 +0,0 @@
-//EJERCICIO DE FOREACH
-
-const alumnos = ["Fausto", "Martin", "Elena"];
-
-alumnos.forEach(name => {
-    console.log("Hola " + name  + ", bienvenido a la clase.");
-});
--- ejercicios_etapa2/ejercicio3.js
+++
@@ -1,8 +0,0 @@
-//EJERCICIO CON FILTER
-//Creá un nuevo array llamado aprobados que solo tenga las notas mayores o iguales a 6 usando .filter().
-
-const notas = [4, 8, 3, 10, 5, 7];
-
-const aprobados = notas.filter((aprobados) => aprobados >= 6);
-
-console.log(aprobados);
