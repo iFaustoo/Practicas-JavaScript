9523f64643e1f51aa3378064e5eca44568e3e1bd
iFaustoo <faustoordoriguez10@gmail.com>
2026-05-15 13:18:42 -0300

    -

---
+++ ejercicios_etapa2/ejercicio23.js
@@ -0,0 +1,10 @@
+//EJERCICIO ORDENAR POR PROPIEDAD
+//Tarea: Usá el método .sort() para ordenar el array de mayor a menor según la nota.
+//Ojo: El .sort() de JS es medio mañoso (ordena como texto por defecto), vas a necesitar
+//pasarle una función de comparación: (a, b) => b.nota - a.nota.
+
+const alumnos = [
+    { nombre: "Fausto", nota: 10 },
+    { nombre: "Ana", nota: 7 },
+    { nombre: "Pedro", nota: 9 }
+];
